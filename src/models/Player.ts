3cff19ab811267df6449c249c1ff357a5b2dcff3
Simon Husser <simon.husser2@etu.unistra.fr>
2025-01-05 18:44:51 +0100

    player cane have multiple cards and has a displayFunction

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -2,10 +2,20 @@ import { Card } from "./cards/Card";
 
 export class Player {
   name: string;
-  card: Card;
+  cards: Card[] = [];
 
-  constructor(name: string, card: Card) {
+  constructor(name: string, cards: Card[]) {
     this.name = name;
-    this.card = card;
+    this.cards = cards;
+  }
+
+  /**
+   * Display the player's name and his card
+   */
+  toString(): void {
+    console.log(`${this.name} a en main :`);
+    this.cards.forEach((card) => {
+      console.log(`- ${card.name}`);
+    });
   }
 }
